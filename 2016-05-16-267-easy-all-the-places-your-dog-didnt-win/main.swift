a3f8fb6baa394617d4348d065339e332eda22d4c
Paul Calotta <paul.calotta@gmail.com>
2019-07-01 19:33:54 -0400

    Fixed incorrect return value in Swift solution for 2016-05-16

--- 2016-05-16-267-easy-all-the-places-your-dog-didnt-win/main.swift
+++ 2016-05-16-267-easy-all-the-places-your-dog-didnt-win/main.swift
@@ -2,7 +2,7 @@ import Foundation
 
 func otherPlaces(forPlace place: Int) -> String {
     let suffixes: [String] = ["st", "nd", "rd"]
-    let ret = (1...100).map { (i: Int) -> String in
+    let ret = (0...100).map { (i: Int) -> String in
         if i == place {
             return ""
         }
